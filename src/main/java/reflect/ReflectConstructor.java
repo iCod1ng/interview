12d1316530ac19580bdd8868cbf5900fdc59ce5f
NOFX <yyc95102@vip.qq.com>
2020-04-06 17:28:53 +0800

    反射-constructor.newInstance()

--- src/main/java/reflect/ReflectConstructor.java
+++ src/main/java/reflect/ReflectConstructor.java
@@ -1,6 +1,7 @@
 package reflect;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author yanyuchi
@@ -8,14 +9,18 @@ import java.lang.reflect.Constructor;
  */
 @SuppressWarnings("unchecked")
 public class ReflectConstructor {
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
         Class personClass = Person.class;
         Constructor constructor = personClass.getConstructor(String.class,String.class);
         System.out.println(constructor);
 
         System.out.println("-----");
         Object person = personClass.newInstance();
+
+        //通过Constructor的newInstance可以使用有参构造新的对象
+        Object person1 = constructor.newInstance("bb","88");
         System.out.println(person);
+        System.out.println(person1);
 
 
     }
