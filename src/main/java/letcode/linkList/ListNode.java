e1e46c5d9c6c37b33988d3da8ca5de4aee0fa81b
NOFX <yyc95102@vip.qq.com>
2020-07-22 13:26:53 +0800

    Letcode 链表相交

---
+++ src/main/java/letcode/linkList/ListNode.java
@@ -0,0 +1,66 @@
+package letcode.linkList;
+
+/**
+ * 节点定义
+ * @author yanyuchi
+ * @date 2020-07-22 10:11
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(){}
+
+    public ListNode(int x){
+        val = x;
+        next = null;
+    }
+
+    /**
+     * 无头节点尾插法
+     * @param a 数组
+     */
+    public static ListNode  createListR(int a[]){
+
+        ListNode header = null;
+        ListNode r = null;
+        int i;
+        if(a.length>0){
+            for(i=0;i<a.length;i++){
+                ListNode s = new ListNode(a[i]);
+                if(header == null){
+                    header = s;
+                    r = s;
+                }else {
+                    r.next = s;
+                    r = r.next;
+                }
+            }
+            r.next = null;
+        }
+        return header;
+    }
+
+    public static void printLinkList(ListNode header){
+        while (header!=null){
+            System.out.println(header.val);
+            header = header.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        int a [] = {4,1,2,3};
+        ListNode header = createListR(a);
+        while (header!=null){
+            System.out.println(header.val);
+            header = header.next;
+        }
+    }
+}
---
+++ src/main/java/letcode/linkList/easy/intersect/Solution.java
@@ -0,0 +1,86 @@
+package letcode.linkList.easy.intersect;
+
+import letcode.linkList.ListNode;
+
+/**
+ * @author yanyuchi
+ * @date 2020-07-22 10:16
+ */
+public class Solution {
+
+
+    public ListNode getIntersectionNode(ListNode headA,ListNode headB){
+        if(headA == null || headB == null){
+            return  null;
+        }
+        ListNode p =  headA;
+        ListNode q = headB;
+        int lengthA = getLength(p);
+        int lengthB = getLength(q);
+        if(lengthA>lengthB){
+            int offset = lengthA - lengthB;
+            for(int i=0;i<offset;i++){
+                headA = headA.next;
+            }
+        }else {
+            int offset = lengthB - lengthA;
+            for(int i=0;i<offset;i++){
+                headB = headB.next;
+            }
+        }
+        while (headA!=null && headB !=null){
+            if(headA==headB){
+                System.out.println("Intersect Node:"+headA);
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        System.out.println("No intersect");
+        return null;
+    }
+
+    public int getLength(ListNode node){
+        int length = 0;
+        while (node!=null){
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        int a [] = {4,1,8,4,5};
+        int b [] = {5,0,1};
+
+        ListNode headerA = ListNode.createListR(a);
+
+        ListNode headerB = ListNode.createListR(b);
+
+        ListNode p = headerA;
+
+        while (p!=null){
+            if(p.val == 8){
+                break;
+            }
+            p = p.next;
+        }
+        ListNode q = headerB;
+        while (q!=null){
+            if(q.next == null){
+                q.next = p;
+                break;
+            }
+            q = q.next;
+        }
+
+        ListNode.printLinkList(headerA);
+        ListNode.printLinkList(headerB);
+
+        Solution solution = new Solution();
+
+        solution.getIntersectionNode(headerA,headerB);
+
+    }
+
+}
